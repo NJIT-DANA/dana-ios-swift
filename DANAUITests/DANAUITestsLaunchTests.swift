45e66010a3f1ccb78fd0388a55e776dea5886b73
Littman Library <littmanlibrary@Littmans-MBP.home>
2022-02-05 12:46:07 -0500

    initial commit -ios

---
+++ DANAUITests/DANAUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DANAUITestsLaunchTests.swift
+//  DANAUITests
+//
+//  Created by Littman Library on 2/5/22.
+//
+
+import XCTest
+
+class DANAUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
---
+++ Podfile
@@ -0,0 +1,10 @@
+
+# Uncomment the next line to define a global platform for your project
+
+
+
+target 'DANA' do
+
+  use_frameworks!
+    pod 'Alamofire', '~> 5.0.0-rc.2'
+end
