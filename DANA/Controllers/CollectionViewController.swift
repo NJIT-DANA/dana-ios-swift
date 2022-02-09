3bb12df87f773bbf9fcb35c468d725a4fd029f9c
Rini Joseph <>
2022-02-09 02:53:21 -0500

    render/optimise

--- DANA/Controllers/CollectionViewController.swift
+++ DANA/Controllers/CollectionViewController.swift
@@ -9,96 +9,53 @@ import UIKit
 import CoreData
 
 class CollectionViewController: UIViewController {
-let currentNetworkmanager = networkManager()
-var architectViewModel = ArchitectsViewModel()
+    let currentNetworkmanager = networkManager()
+    var architectViewModel = ArchitectsViewModel()
     
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            
-            self.setUI()
-            if self.architectViewModel.checkArchitectsisEmpty(entity: textConstants.architectEntity){
-                self.fetchArchitectsfromAPI()
-            }
-           // self.fetchImagesofArchitects()
-           
-            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.setUI()
+        if self.architectViewModel.checkArchitectsisEmpty(entity: textConstants.architectEntity){
+            self.fetchArchitectsfromAPI()
+        }
     }
     
-    func test() {
-       
-    }
-//    func operationQueueforAPI(){
-//        let apiOperation = BlockOperation()
-//        let indicatorView = danaHelper.activityIndicator(style: .large,
-//                                                       center: self.view.center)
-//        DispatchQueue.main.async {
-//        self.view.addSubview(indicatorView)
-//        indicatorView.startAnimating()
-//        }
-//
-//        apiOperation.addExecutionBlock {
-//            if self.architectViewModel.checkArchitectsisEmpty(entity: textConstants.architectEntity){
-//                self.fetchArchitectsfromAPI()
-//            }
-//            print("rini here 1")
-//        }
-//        let apiOperation2 = BlockOperation()
-//        apiOperation2.addExecutionBlock{
-//            print("rini here 2")
-//            DispatchQueue.main.async {
-//            self.view.addSubview(indicatorView)
-//            indicatorView.stopAnimating()
-//            }
-//        }
-//
-//
-////        apiOperation.completionBlock = {
-////            DispatchQueue.main.async {
-////            indicatorView.stopAnimating()
-////            }
-////        }
-//
-//        let operationQueue = OperationQueue()
-////        operationQueue.addOperation(apiOperation)
-//        operationQueue.addOperations([apiOperation,apiOperation2], waitUntilFinished: true)
-//
-//    }
-//
     
     func setUI() {
         self.navigationItem.title = textConstants.collectionTitle
         self.navigationController?.navigationBar.backgroundColor = UIColor.black
     }
-
+    
     
     //MARK:- architect Methods
     
     func fetchArchitectsfromAPI(){
         let indicatorView = danaHelper.activityIndicator(style: .large,
-                                                       center: self.view.center)
+                                                         center: self.view.center)
         DispatchQueue.main.async {
-        self.view.addSubview(indicatorView)
-        indicatorView.startAnimating()
+            self.view.addSubview(indicatorView)
+            indicatorView.startAnimating()
         }
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
         architectViewModel.makeFetchArchitectsApiCall(context: context) {
             DispatchQueue.main.async {
-            indicatorView.stopAnimating()
-            print("finally everythings dne")
-            //call api to download the image urls
+                indicatorView.stopAnimating()
+                print("finally everythings dne")
                 self.fetchImagesofArchitects()
-            
             }
+            
             
         }
         
     }
+    
     func fetchImagesofArchitects() {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
         architectViewModel.makeFetchimagesApiCall(context: context) {
-            print("nnn")
+           
         }
     }
 }
--- DANA/ViewModels/ArchitectsViewModel.swift
+++ DANA/ViewModels/ArchitectsViewModel.swift
@@ -11,18 +11,18 @@ import CoreData
 
 
 class ArchitectsViewModel: ObservableObject {
- var architects : [ArchitectsModel] = []
- var imageUrlArray : [ImageModel] = []
- let currentNetworkmanager = networkManager()
+    var architects : [ArchitectsModel] = []
+    var imageUrlArray : [ImageModel] = []
+    let currentNetworkmanager = networkManager()
     
-
+    
     func makeFetchArchitectsApiCall(context: NSManagedObjectContext,completionHandler: @escaping () -> (Void)){
         currentNetworkmanager.fetchArchitectsfromDANA { architectsArray in
             let architectsArrayforDB = architectsArray;
             DispatchQueue.main.async {
-            self.architects = architectsArrayforDB
-            self.saveData(context: context)
-                    }
+                self.architects = architectsArrayforDB
+                self.saveData(context: context)
+            }
             completionHandler()
         }
         
@@ -32,21 +32,20 @@ class ArchitectsViewModel: ObservableObject {
         let architects = fetchallArchitectsfromDB()
         for architect in architects {
             if let imageUrl = architect.fileUrl{
-                
                 currentNetworkmanager.fetchImagefromDANA(imageUrl) { imageurlsArray in
                     let urlArrayforDB = imageurlsArray;
                     DispatchQueue.main.async {
-                    self.imageUrlArray = urlArrayforDB
-                   // self.saveData(context: context)
+                        self.imageUrlArray = urlArrayforDB
+                        // self.saveData(context: context)
                         self.saveImageData(context: context)
-                            }
+                    }
                     completionHandler()
                 }
+            }
+            completionHandler()
         }
-        completionHandler()
     }
-    }
-
+    
     
     //saving data to core data
     func saveData(context: NSManagedObjectContext){
@@ -55,16 +54,14 @@ class ArchitectsViewModel: ObservableObject {
             entity.id = Int32(data.id)
             entity.fileUrl = data.files.url
             entity.name = data.element_texts[0].text
-            //entity.imageUrl = data.url
-    }
-
-    do{
-        try context.save()
-        print("success, saved")
-    }
-    catch{
-        print(error.localizedDescription )
-    }
+        }
+        do{
+            try context.save()
+            print("success, saved")
+        }
+        catch{
+            print(error.localizedDescription )
+        }
     }
     
     //saving imagedata to core data
@@ -79,22 +76,17 @@ class ArchitectsViewModel: ObservableObject {
                     entity.fileUrl = dataArch.fileUrl
                     entity.name = dataArch.name
                     
+                }
             }
         }
         
-//        imageUrlArray.forEach{(data) in
-//            let entity = Architects_Firms(context:context)
-//            entity.id = Int32(data.id)
-//            entity.imageUrl = data.file_urls.original
-    }
-
-    do{
-        try context.save()
-        print("success, saved")
-    }
-    catch{
-        print(error.localizedDescription )
-    }
+        do{
+            try context.save()
+            print("success, saved")
+        }
+        catch{
+            print(error.localizedDescription )
+        }
     }
     
     
@@ -102,46 +94,38 @@ class ArchitectsViewModel: ObservableObject {
     
     //fetch all from DB
     func fetchallArchitectsfromDB()-> Array<Architects_Firms>{
-
-        //let moc = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
-              let appDelegate = UIApplication.shared.delegate as! AppDelegate
-              let context = appDelegate.persistentContainer.viewContext
-
-                let fetchedArchitects = NSFetchRequest<NSFetchRequestResult>(entityName: "Architects_Firms")
-
-                do {
-                    let fetchedArchitects = try context.fetch(fetchedArchitects) as! [Architects_Firms]
-                    print(fetchedArchitects.count)
-                   // print(fetchedArchitects[0])
-                    return fetchedArchitects
-                } catch {
-                    fatalError("Failed to fetch categories: \(error)")
-                }
-
-
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let fetchedArchitects = NSFetchRequest<NSFetchRequestResult>(entityName: textConstants.architectEntity)
+        do {
+            let fetchedArchitects = try context.fetch(fetchedArchitects) as! [Architects_Firms]
+            print(fetchedArchitects.count)
+            return fetchedArchitects
+        } catch {
+            fatalError("Failed to fetch categories: \(error)")
+        }
     }
- 
+    
+    
+    //check if  DB is empty for architects
     func checkArchitectsisEmpty(entity:String)->Bool {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
         let request = NSFetchRequest<NSFetchRequestResult>(entityName: entity)
         var isEmpty = false
-        //let predicate = NSPredicate(format: "name == %@", theName)
-        //request.predicate = predicate
         request.fetchLimit = 1
-
         do{
             let count = try context.count(for: request)
             if(count == 0){
-            isEmpty = true;
+                isEmpty = true;
             }
             else{
                 isEmpty = false;
             }
-          }
+        }
         catch let error as NSError {
-             print("Could not fetch \(error), \(error.userInfo)")
-          }
+            print("Could not fetch \(error), \(error.userInfo)")
+        }
         return isEmpty
     }
 }
